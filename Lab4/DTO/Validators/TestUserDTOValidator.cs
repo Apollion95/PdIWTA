94ea229ebc6e8b5abd2e7c6965bbbbc0d2e3da89
Apollion95 <54260979+Apollion95@users.noreply.github.com>
2022-05-22 13:01:04 +0200

    Initial commit

---
+++ Lab4/DTO/Validators/TestUserDTOValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace Lab4.DTO.Validators
+{
+    public class TestUserDTOValidator : AbstractValidator<TestUserDTO>
+    {
+        public TestUserDTOValidator()
+        {
+            RuleFor(x => x.Email).NotEmpty().MinimumLength(2).MaximumLength(30);
+            RuleFor(x => x.Phone).NotEmpty().Length(9);
+        }
+    }
+}
